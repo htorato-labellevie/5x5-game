bf0e57e0d369d5140f3de2bfadb75fb3cc9a1f3b
Hirofumi Torato <htorato@labellevie.jp>
2025-04-23 12:58:20 +0900

    chore: update drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,15 +1,10 @@
-// frontend/drizzle.config.ts
-import { defineConfig } from 'drizzle-kit';
+import type { Config } from 'drizzle-kit';
 
-export default defineConfig({
+export default {
   schema: './src/db/schema.ts',
   out: './drizzle',
-  dialect: 'postgresql',
+  dialect: 'postgresql', // ✅ ここは必要
   dbCredentials: {
-    host: 'dpg-d045dsi4d50c739rg4qg-a.oregon-postgres.render.com',
-    port: 5432,
-    user: 'gomoku_db_user',
-    password: 'Nk3MAgtztAJ6HsasxjSSvmpBBRp1zM6F',
-    database: 'gomoku_db',
+    url: process.env.DATABASE_URL!,
   },
-});
+} satisfies Config;
